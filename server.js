04e1f78f8fb87e04048d1569ca38889990542c87
Mauro Scomparin <scompo@gmail.com>
2017-08-20 20:46:29 +0200

    server nicer.

--- server.js
+++ server.js
@@ -4,27 +4,29 @@ const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
   type Query {
-    	hello: String
-	prova: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 const root = {
-	hello: () => {
-		return "World?";
-	},
-	prova: () => {
-		return "eh si!";
-	}
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
 };
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
-	schema: schema,
-	rootValue: root,
-	graphiql: true,
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
 }));
-
 app.listen(3000);
-console.log('GraphQL API running at: http://localhost:3000/graphql');
-
+console.log('Running a GraphQL API server at http://localhost:3000/graphql');

24c8fbd26535a97ab8b806eb3c9a490ada1f3254
Mauro Scomparin <scompo@gmail.com>
2017-08-20 20:35:54 +0200

    aggiunto server

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    	hello: String
+	prova: String
   }
 `);
 
 const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+	hello: () => {
+		return "World?";
+	},
+	prova: () => {
+		return "eh si!";
+	}
 };
 
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+const app = express();
+app.use('/graphql', graphqlHTTP({
+	schema: schema,
+	rootValue: root,
+	graphiql: true,
+}));
+
+app.listen(3000);
+console.log('GraphQL API running at: http://localhost:3000/graphql');
+

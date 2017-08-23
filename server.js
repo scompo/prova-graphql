4d7ee63c82df083faaecb81423c8e4cfe1892079
Mauro Scomparin <scompo@gmail.com>
2017-08-23 18:02:45 +0200

    tmp

--- server.js
+++ server.js
@@ -1,25 +1,93 @@
+const PORT = 4000;
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const {
-  buildSchema
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
 } = require('graphql');
 
-const PORT = 4000;
+const Sequelize = require('sequelize');
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
+const sequelize = new Sequelize('prova-graphql', 'niente', 'niente', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: '/home/scompo/programmi/prova-graphql/prova-graphql.sqlite'
+});
+
+const MioType = new GraphQLObjectType({
+  name: 'mio',
+  fields: () => ({
+    a: {
+      type: GraphQLString
+    },
+    b: {
+      type: GraphQLString
+    }
+  })
+});
+
+const MioModel = sequelize.define('mio', {
+  a: {
+    type: Sequelize.STRING
+  },
+  b: {
+    type: Sequelize.STRING
   }
-`);
+});
 
-const root = {
-  hello: () => 'Hello world!'
-};
+MioModel.sync({
+  force: true
+}).then(() => {
+  return MioModel.create({
+    a: 'aaaa',
+    b: 'bbbb'
+  })
+});
+
+const mioResolver = (source, args, context) => {
+  return MioModel.findOne().then(m => {
+    return m
+  });
+}
+
+const rootQuery = new GraphQLObjectType({
+  name: 'query',
+  fields: {
+    mio: {
+      type: MioType,
+      resolve: mioResolver
+    }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: rootQuery
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const app = express();
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
-  graphiql: true,
+  graphiql: true
 }));
+
 app.listen(PORT, () => console.log(`server running on to http://localhost:${PORT}/graphql`));
